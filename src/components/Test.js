ac5a3b251c8c58a56cb0993c637ee62f5e9604c8
Bruce Wang <bruce.wang15@outlook.com>
2024-09-15 21:20:42 -0400

    pawns, knight, bishop, and rook finished. En passant and queen left. There may be some glitches for taking turns.

--- src/components/Test.js
+++ src/components/Test.js
@@ -17,9 +17,12 @@ const Test = () => {
     const playerTurnRef = useRef(playerTurn);
     useEffect(() => {
         playerTurnRef.current = playerTurn;
+
     }, [playerTurn]);
 
     const dragStart = (e) => {
+
+
 
         draggedElement=e.target.parentNode.parentNode //the square
         startPositionId = e.target.parentNode.parentNode
@@ -32,46 +35,147 @@ const Test = () => {
 
     }
     const dragDrop = (e) => {
+
+        /*
+
+        To clarify:
+        If landing on square:
+        -e.target = the square
+
+        If landing on piece:
+        -e.target= pieceImg
+
+         */
+
         e.stopPropagation()
 
         const correctPlayer = draggedElement.firstChild.classList.contains(playerTurnRef.current)
 
+
+
         const opponentTurn = playerTurnRef.current === 'white' ? 'black': 'white'
-        console.log(correctPlayer, draggedElement.firstChild.classList, playerTurnRef.current)
         const takenByOpponent = e.target.classList.contains(opponentTurn) // check if opp piece occupies that square
         const taken = e.target.classList.contains(playerTurnRef.current) //checks if friendly piece occupies that square
-        console.log(taken, takenByOpponent, opponentTurn, e.target, 'this')
+
 
         const valid = checkIfValid(e.target)
+        console.log(valid, 'valid2')
         if (correctPlayer) {
 
             if (valid && takenByOpponent) {
-                console.log(e.target, "e.target")
+
                 e.target.parentNode.parentNode.append(draggedElement.firstChild)
                 e.target.parentNode.parentNode.removeChild(e.target.parentNode)
+                changePlayer()
             }
             if (taken) {
                 console.log('you cant go here')
             }
             if (valid) {
                 e.target.append(draggedElement.firstChild)
+                console.log('append done')
+                changePlayer()
             }
-            changePlayer()
+
 
 
         }
 
     }
     const checkIfValid = (target) => {
-        const targetId = target.id || target.parentNode.parentNode.id
-        const startId = startPositionId.id
+        const targetId = Number(target.id) || Number(target.parentNode.parentNode.id)
+        const startId = Number(startPositionId.id)
         const piece = draggedElement.firstChild.id
-        console.log(targetId, startId, piece, 'valid')
+        console.log(targetId, startId, piece, startId+16)
 
+
+        let dir
         switch(piece) {
-            case 1:
-                const piece = draggedElement.firstChild.id
+
+            case 'pawn':
+
+                const starterRow = [8, 9, 10, 11, 12, 13, 14, 15]
+
+                if (starterRow.includes(startId) && startId+8*2 === targetId ) {
+                    //pawn moves 2 squares forward
+                    return true
+                }
+                else if (startId + 8 === targetId) {
+                    //pawn moves 1 square after first move
+                    return true
+                }
+                return false
+            case 'knight':
+                if (
+                    startId+17 === targetId || startId+10 === targetId ||
+                    startId-17 === targetId || startId-10 === targetId ||
+                    startId+15 === targetId || startId+6 === targetId ||
+                    startId-15 === targetId || startId-6 === targetId) {
+                    return true
+                }
+            case 'king':
+                if (
+                    startId+1 === targetId || startId-1===targetId ||
+                    startId+8 === targetId || startId-8===targetId ||
+                    startId+9 === targetId || startId-9===targetId ||
+                    startId+7 === targetId || startId-7===targetId
+                ) {
+
+                    return true
+                }
+            case 'bishop':
+                const maxPieceGoThrough = 0;
+                let piecesInWay = 0;
+                if ((targetId-startId) % 9 !== 0 && (targetId-startId) % 7 !== 0) {
+                    console.log('invalid bishop move')
+                    return false
+
+
+                }
+
+                if ((targetId-startId) % 9 === 0) {
+                    dir = (targetId-startId)/Math.abs(targetId-startId)*9
+                }
+                if ((targetId-startId) % 7 === 0) {
+                    dir = (targetId-startId)/Math.abs(targetId-startId)*7
+                }
+                console.log(dir, 'dir')
+                if (checkForPiecesInWay(dir, startId, targetId)) return false
+                return true
+
+
+
+            case 'queen':
+
+            case 'rook':
+                if ((targetId-startId) % 8 !== 0 && Math.abs(targetId-startId) > 8) {
+                    console.log('bad rook move')
+                    return false
+                }
+
+                if ((targetId-startId) % 8 === 0) {
+                    dir = (targetId-startId)/Math.abs(targetId-startId)*8
+                }
+                else {
+                    dir = 1
+                }
+                return true
         }
+    }
+    const checkForPiecesInWay = (dir, startId, targetId) => {
+
+        for (let i = 1; i < Math.abs((targetId-startId)/dir); i++) {
+            console.log(startId+i*dir)
+            let eachSquare = document.getElementById(`${startId+i*dir}`)
+            console.log(eachSquare, 'eachSquare')
+            if (eachSquare.firstChild?.classList.contains('piece')) {
+                console.log('piece in the way', dir)
+                return true; //if piece in way
+            }
+        }
+        return false; //if piece not in way
+
+
     }
 
 
@@ -83,6 +187,16 @@ const Test = () => {
         changePlayerTurn(prevTurn => prevTurn === 'black' ? 'white' : 'black');
 
         console.log(playerTurn, 'after')
+        flipSquareID()
+    }
+
+    const flipSquareID = () => {
+        const temp = document.getElementById('chessBoard')
+        for (let i = 0; i < temp.children.length; i++) {
+            for (let k = 0; k < temp.children[i].children.length; k++) {
+                temp.children[i].children[k].id = Number(63-Number(temp.children[i].children[k].id))
+            }
+        }
     }
 
     useEffect(() => {
@@ -102,7 +216,7 @@ const Test = () => {
 
                 const square = document.createElement('div')
                 square.className = `Cell ${BlackWhite}Square`
-                square.id = `${63-added}`
+                square.id = 63-added
 
                 square.addEventListener('dragstart', dragStart) //when you click the object
                 square.addEventListener('dragover', dragOver) //when you're dragging and holding it
@@ -119,18 +233,18 @@ const Test = () => {
         }
 
 
-        console.log(grid, piece1.piece[2])
+
     }, [forever])
 
 
     const fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR'
     const pieceDict = {
-        'k': 1,
-        'p': 2,
-        'n': 3,
-        'b': 4,
-        'r': 5,
-        'q': 6,
+        'k': 'king',
+        'p': 'pawn',
+        'n': 'knight',
+        'b': 'bishop',
+        'r': 'rook',
+        'q': 'queen',
     }
     const FENtoGrid = (FEN) => {
 
@@ -157,12 +271,13 @@ const Test = () => {
 
 
 
-                    const temp = document.getElementById(`${String((7-rank)*8+file)}`)
-
+                    const temp = document.getElementById(`${String(63-((7-rank)*8+file))}`)
+                    console.log(temp.id, 'id')
 
                     const temp2 = new Pieces(whatColor, pieceDict[fen[i].toLowerCase()])
                     const piece = document.createElement('div')
                     const pieceImg = document.createElement('img')
+
 
 
 
